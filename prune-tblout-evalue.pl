375e5b73bd7b08ff7612df73580e6eb37fcb4eac
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2018-07-20 06:44:56 -0400

    prune-tblout-evalue.pl now can prune based on bit score

--- prune-tblout-evalue.pl
+++ prune-tblout-evalue.pl
@@ -1,8 +1,14 @@
-$usage = "perl prune-tblout-evalue.pl <Infernal v1.1x tblout file> <E-value threshold>";
+$usage = "perl prune-tblout-evalue.pl <Infernal v1.1x tblout file> <'S' or 'E' for bit score or E-value threshold> <threshold>";
 
-if(scalar(@ARGV) != 2) { die $usage; }
+if(scalar(@ARGV) != 3) { die $usage; }
 
-($tblout_file, $ecut) = (@ARGV);
+($tblout_file, $s_or_e, $thresh) = (@ARGV);
+
+if(($s_or_e ne "S") && ($s_or_e ne "E")) { 
+  die "ERROR second cmd line argument must be 'S' (for bit score threshold) or 'E' (for E-value threshold)"
+}
+
+$do_bitsc = ($s_or_e eq "S") ? 1 : 0;
 
 open(TBLOUT, $tblout_file) || die "ERROR unable to open $tblout_file";
 
@@ -14,9 +20,8 @@ while($line = <TBLOUT>) {
 #gi|255927625|gb|GQ303260.1| -         sim.all.YrlA.396.2   -          cm        1      105   128177   128297      +    no    1 0.58   0.0   32.1   0.00015 !   Mycobacterium phage ET08, complete genome
     @el_A = split(/\s+/, $line);
     if(scalar(@el_A) < 18) { die "ERROR less than 18 tokens on line $line"; }
-    if($el_A[15] <= $ecut) { 
-      print $line . "\n";
-    }
+    if   ((  $do_bitsc) && ($el_A[14] >= $thresh)) { print $line . "\n"; }
+    elsif((! $do_bitsc) && ($el_A[15] <= $thresh)) { print $line . "\n"; }
   }
 }
 
