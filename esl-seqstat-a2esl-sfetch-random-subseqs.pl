2337deb71c89b2670ddf5fa1b6bd6e43ca97816d
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2022-10-26 12:11:10 -0400

    Adds --seed and --min options

--- esl-seqstat-a2esl-sfetch-random-subseqs.pl
+++ esl-seqstat-a2esl-sfetch-random-subseqs.pl
@@ -13,15 +13,35 @@ use Getopt::Long;
 my $usage;
 $usage  = "esl-seqstat-a2esl-sfetch-random-subseqs.pl\n\n";
 $usage .= "Usage:\n\n";
-$usage .= "esl-seqstat-a2esl-sfetch-random-subseqs.pl <esl-seqstat -a output file> <length of subseqs (-1 for full seqs)> <number of subseqs>\n\n";
+$usage .= "esl-seqstat-a2esl-sfetch-random-subseqs.pl [OPTIONS] <esl-seqstat -a output file> <length of subseqs (-1 for full seqs)> <number of subseqs>\n\n";
+$usage .= "\tOPTIONS:\n";
+$usage .= "\t\t--seed <n> : set RNG seed as <n>\n";
+$usage .= "\t\t--min  <n> : randomly sample lengths, and set minimum length as <n>, maximum as 2nd cmdline arg>\n";
+
+my $seed = 42;         # changed if --seed is used
+my $do_sample_len = 0; # set to '1' if --min used
+my $min_len = undef;   # defined if --min used
+
+&GetOptions( "seed=s"    => \$seed,
+             "min=s"     => \$min_len);
 
 if(scalar(@ARGV) != 3) { die $usage; }
 my ($seqstat_file, $target_len, $target_nseq) = @ARGV;
 
+if(($target_len == -1) && (defined $min_len)) { 
+  die "ERROR with --min, 2nd cmdline arg can't be set as -1";
+}
+if((defined $min_len) && ($target_len <= $min_len)) { 
+  die "ERROR with --min <n>, 2nd cmdline arg must be > $min_len";
+}
+
+if(defined $min_len) { 
+  $do_sample_len = 1;
+}
+
 open(IN, $seqstat_file) || die "ERROR unable to open $seqstat_file for reading";
 
-srand(42);
-
+srand($seed);
 
 my $seen_above_target_len = 0;
 my @seqname_A = ();
@@ -64,17 +84,23 @@ while($i < $target_nseq) {
     printf("%s\n", $seqname);
     $i++;
   }
-  elsif($seqlen >= $target_len) { 
-    my $start = rand(($seqlen - $target_len)) + 1;
-    my $stop  = $start + $target_len - 1;
-    if($start < 1)       { die "ERROR got start ($start) < 1 for $seqname, len $seqlen"; }
-    if($stop  > $seqlen) { die "ERROR got stop  ($stop)  > $seqlen for $seqname, len $seqlen"; }
-
-    my $newname = sprintf("%s/%d-%d", $seqname, $start, $stop);
-    if(! defined $newname_H{$newname}) { 
-      printf("%s %d %d %s\n", $newname, $start, $stop, $seqname);
-      $i++;
-      $newname_H{$newname} = 1;
+  else { 
+    my $cur_target_len = $target_len;
+    if($do_sample_len) { 
+      $cur_target_len = $min_len + (rand(($target_len - $min_len) + 1) - 1);
+    }
+    if($seqlen >= $cur_target_len) { 
+      my $start = rand(($seqlen - $cur_target_len)) + 1;
+      my $stop  = $start + $cur_target_len - 1;
+      if($start < 1)       { die "ERROR got start ($start) < 1 for $seqname, len $seqlen"; }
+      if($stop  > $seqlen) { die "ERROR got stop  ($stop)  > $seqlen for $seqname, len $seqlen"; }
+      
+      my $newname = sprintf("%s/%d-%d", $seqname, $start, $stop);
+      if(! defined $newname_H{$newname}) { 
+        printf("%s %d %d %s\n", $newname, $start, $stop, $seqname);
+        $i++;
+        $newname_H{$newname} = 1;
+      }
     }
   }
 }
