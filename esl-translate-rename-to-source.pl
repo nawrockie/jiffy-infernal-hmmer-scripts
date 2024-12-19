3c87fd494755e2abce8e56fe4bd7fb4463cda095
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2024-12-19 10:20:14 -0500

    Usage now prints if no input file provided

--- esl-translate-rename-to-source.pl
+++ esl-translate-rename-to-source.pl
@@ -5,9 +5,9 @@ use Getopt::Long;
 my $usage = "esl-translate-rename-to-source.pl: rename sequences output from esl-translate\n";
 $usage = "USAGE:\nperl esl-translate-longest-orf.pl <esl-translate output file>";
 $usage .= "\tOPTIONS:\n";
-$usage .= "\t\t-o: include orf name in new name\n\t\t";
-$usage .= "\t\t-l: include length in new name\n\t\t";
-$usage .= "\t\t-f: include frame in new name\n\t\t";
+$usage .= "\t\t-o: include orf name in new name\n";
+$usage .= "\t\t-l: include length in new name\n";
+$usage .= "\t\t-f: include frame in new name\n";
 
 my $do_orf    = 0;
 my $do_length = 0;
@@ -16,7 +16,11 @@ my $do_frame  = 0;
              "l" => \$do_length, 
              "f" => \$do_frame);
 
-while(my $line = <>) { 
+if(scalar(@ARGV) != 1) { die $usage; }
+my ($in_file) = (@ARGV);
+open(IN, "<", $in_file) || die "ERROR unable to open file $in_file for reading";
+
+while(my $line = <IN>) { 
   chomp $line;
   if($line =~ m/^\>/) { 
     if($line =~ /^\>(orf\d+)\s+source\=(\S+)\s+coords\=(\d+)\.\.(\d+)\s+length\=(\d+)\s+frame\=(\d+)\s+desc\=(.*)$/) { 
@@ -41,3 +45,4 @@ while(my $line = <>) {
     print $line . "\n";
   }
 }
+close(IN);
